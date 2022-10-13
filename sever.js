68d99e7785718d6e228ac9cd5127930ff6ec93e1
Daniel Alcazar <danielalcazar54@gmail.com>
2022-10-13 00:26:02 -0300

    ADD view forbidden MOD login logout funcional

--- sever.js
+++ sever.js
@@ -10,6 +10,16 @@ const apiProductos = new ProductoMock();
 import { ProductoMock } from './src/mocks/producto.mock.js';
 import { mensajesDao as mensajesApi } from './src/dao/index.js';
 import { schema, normalize } from 'normalizr'
+import session from "express-session";
+import connectMongo from 'connect-mongo';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+//Persistencia session MongoDb Atlas
+const MongoStore = connectMongo.create({
+    mongoUrl: process.env.MONGODB_ATLAS_URL,
+    ttl: 600
+})
 
 //Solucion a __dirname
 const __filename = fileURLToPath(import.meta.url);
@@ -28,12 +38,28 @@ const schMensajes = new schema.Entity('posts', {mensajes: [schMensaje] }, {idAtt
 
 const normalizrMensajes = (msjsId) => normalize(msjsId, schMensajes);
 
-/******Middleware******/
+/******Middlewares******/
 app.use(express.urlencoded({extended:true}));
 app.use(express.static('./public'));
 app.use('/api', express.static('./public'));
 app.use('/login', express.static('./public'));
 app.use(morgan('dev'));
+
+//Session Setup
+app.use(session({
+    store:MongoStore,
+    secret: process.env.SECRET_KEY,
+    resave: true,
+    saveUninitialized: true
+}));
+
+//Session Auth
+function auth (req, res, next) {
+    if (req.session.user){
+        return next()
+    }
+    return res.render('forbidden.hbs')
+}
 
 //Motores de plantillas
 //HBS
@@ -47,18 +73,37 @@ app.set('view engine', 'hbs');
 app.set('views','./src/views');
 
 /******Rutas******/
-app.get('/', (req,res)=>{
-    res.render('main', {test:false})
+app.get('/', auth, (req,res)=>{
+    const usuario = req.session.user;
+    res.render('main', {test:false, firstname: usuario})
 });
 
-app.get('/api/productos-test', (req,res)=>{
+app.get('/api/productos-test', auth, (req,res)=>{
     const productos = apiProductos.almacenar(apiProductos.generarProducto())
-    res.render('main', {test:true , api:productos})
+    const usuario = req.session.user;
+    res.render('main', {test:true , api:productos, firstname: usuario})
 });
 
 app.get('/login', (req,res)=>{
     res.render('login.hbs')
-})
+});
+
+app.post('/login', (req,res)=>{
+    const userName = req.body.usuario;
+    req.session.user = userName;
+    res.redirect('/');
+});
+
+app.get('/logout', (req,res)=>{
+    const usuario = req.session.user;
+    req.session.destroy(err=>{
+        if (err){
+            res.json({err});
+        }else{
+            res.render('logout.hbs', {firstname: usuario})
+        }
+    })
+});
 
 /******Web Socket******/
 //Chat
